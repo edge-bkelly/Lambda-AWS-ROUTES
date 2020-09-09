f89b1b15ea073a422af114fbc3228beb752ada3a
Ben Kelly <Ben@ben-home.co.uk>
2020-09-10 00:07:50 +0100

    first commit

---
+++ dict-test.py
@@ -0,0 +1,19 @@
+
+
+
+routes = {'london' : {'rt': 'rtb-03037d64a067051df', 'gw': 'igw-0a372839e84ff8920'},
+{'nyc' : {'rt': 'rtb-06b53b7ca44627078', 'gw': 'igw-0a372839e84ff8920'}}
+
+#for loc in routes:
+
+    #gw = dict['gw']
+    #rt = dict['rt']
+    #print(rt)
+    #print(gw)
+routes2 = {'name': 'ben', 'surname': 'kelly'}
+for i in routes2:
+    print(i)
+
+
+routes = {'london' : {'rt': 'rtb-03037d64a067051df', 'gw': 'igw-0a372839e84ff8920'},
+'nyc' : {'rt': 'rtb-06b53b7ca44627078', 'gw': 'igw-0a372839e84ff8920'}}
---
+++ routes.py
@@ -0,0 +1,45 @@
+ def add_route(rt,gw,reg):
+        ec2 = boto3.resource('ec2', region_name=reg)
+        route_table = ec2.RouteTable(rt)
+        route_table.create_route(
+        DestinationCidrBlock='1.1.1.1/32',
+        GatewayId= gw
+        )
+
+    routes = {'london': {'rt': 'rtb-058532a752007b308', 'gw': 'igw-0a372839e84ff8920', 'reg': 'eu-west-2'},
+    'nyc' : {'rt': 'rtb-06b53b7ca44627078', 'gw': 'igw-0a372839e84ff8920', 'reg': 'eu-west-2'},
+    'ohio' : {'rt': 'rtb-c875aaa3', 'gw': 'igw-5c2a9a34', 'reg': 'us-east-2'}}
+
+    for loc, dict in routes.items():
+        gw = dict['gw']
+        rt = dict['rt']
+        reg = dict['reg']
+        add_route(rt,gw,reg)
+
+    return None
+
+
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+    def add_route(rt,gw,reg):
+           ec2 = boto3.resource('ec2', region_name=reg)
+           route_table = ec2.RouteTable(rt)
+           route_table.create_route(
+           DestinationCidrBlock='1.1.1.1/32',
+           GatewayId= gw
+           )
+
+    routes = {'London-1': {'rt': 'rtb-058532a752007b308', 'gw': 'igw-0a372839e84ff8920', 'reg': 'eu-west-2'},
+    'London-2' : {'rt': 'rtb-06b53b7ca44627078', 'gw': 'igw-0a372839e84ff8920', 'reg': 'eu-west-2'},
+    'Ohio' : {'rt': 'rtb-c875aaa3', 'gw': 'igw-5c2a9a34', 'reg': 'us-east-2'}}
+
+    for loc, dict in routes.items():
+        gw = dict['gw']
+        rt = dict['rt']
+        reg = dict['reg']
+        add_route(rt,gw,reg)
+
+    return None
